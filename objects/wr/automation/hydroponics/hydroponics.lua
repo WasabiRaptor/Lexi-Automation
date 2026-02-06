fccfbbac82ef2e0bcbea1c5d5ac96e390b1828a8
WasabiRaptor <ketchupraptor@gmail.com>
2026-02-06 17:40:32 -0500

    default image plants will refresh their image if it gets added

--- objects/wr/automation/hydroponics/hydroponics.lua
+++ objects/wr/automation/hydroponics/hydroponics.lua
@@ -8,16 +8,20 @@ function init()
 	animator.setPartTag("leftPlant", "variant", tostring(storage.leftPlantVariant or 1))
 	animator.setPartTag("rightPlant", "partImage", storage.plantImage or "")
 	animator.setPartTag("rightPlant", "variant", tostring(storage.rightPlantVariant or 1))
+	if (storage.plantImage == "animationParts/plants/default.png") and storage.plantName and plantImages[storage.plantName] then
+		setPlantImage(storage.plantName)
+	end
 end
 
 function setPlantImage(plantName)
+	storage.plantName = plantName
 	if plantName then
 		if plantImages[plantName] then
 			storage.plantImage = plantImages[plantName].image
 			storage.leftPlantVariant = math.random(plantImages[plantName].variants)
 			storage.rightPlantVariant = math.random(plantImages[plantName].variants)
 		else
-			storage.plantImage = "animationParts/default.png"
+			storage.plantImage = "animationParts/plants/default.png"
 			storage.leftPlantVariant = math.random(5)
 			storage.rightPlantVariant = math.random(5)
 		end
